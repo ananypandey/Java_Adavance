5568a38b7777c2fb60696d9492acfbc7f72d72ae
Pandey, Anany <anany.pandey@dell.com>
2023-11-29 19:45:50 +0530

    Java 8

--- JAVA-8/src/main/java/HashMapss/Hashing_Mapping.java
+++ JAVA-8/src/main/java/HashMapss/Hashing_Mapping.java
@@ -1,2 +1,84 @@
-package HashMapss;public class Hashing_Mapping {
+package HashMapss;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Hashing_Mapping
+{
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for(int i=0;i<10;i++)
+        {
+            hashMap.put(i,(int)(Math.random()*30));
+        }
+        //Types of Map in JAVA
+        //1. HashMap.
+        //2. HashTable.
+        //3. TreeHashMap.
+        //4. LinkedHashMap.
+
+        //Traversing the HashMap
+        Set<Map.Entry<Integer,Integer>> values = hashMap.entrySet();
+        for(Map.Entry<Integer,Integer> e : values)
+        {
+            System.out.println(e.getKey()+ " "+e.getValue());
+        }
+        //Difference Between HashMap and HashTable
+        //HashMap :
+        //It is not thread safe and unsynchronized
+        //It is fast
+        //Works with single thread
+        //Allows one null key
+
+        //HashTable :
+        //It is thread safe and is synchronized
+        //It is slow
+        //Works with multiple threads
+        //Does not allow null key.
+
+        //Linked HashMap has insertion order fixed
+
+        //Tree HashMap stores the input in sorted fashion.
+
+        //Map Working :
+        //Map<String,Integer> marks = new HashMap<String,Integer>();
+        //marks.put("Naveen",100);
+        //marks.put("Tom",200);
+        //marks.put("Lisa",300);
+        //marks.put("Peter,400);
+        //marks.put("Robby",600);
+
+        //Each entry node of the map will contain
+        //Node<K,V>
+        //int hash
+        //K key
+        //V value
+        //Node<K,V> next
+
+
+        //put implementation in JAVA :
+        //public V put(K key, V value){
+        //int hash = hashCode(key) --> Here the hash code for the given input is calculated.
+        //int index = hash * (n-1) --> n here denotes the hash map size.
+        //}
+
+        // If the hashCode of the two key are same that means they are needed to be inserted in the index which leads to collision.
+        // So in case of collision we add the new map entry node in a link list pattern. Making Node<K,V> next point to collision block.
+
+        //For the get operation
+        //hashcode will be computed to get the index value. In case there are more than one node present for a given key it will traverse the list present on that index and
+        //perform equalsTo operation node attribute key.
+
+        //For the insertion such as marks.put(null,300)
+        // Index is zero;
+
+        //What if for every entry hashcode is getting generated same :
+        // Disadvantage : Leads to increase in Time Complexity.
+        // To address this issue, JAVA 8 hash elements use balanced tree instead f linked list after certain threshold is reached.
+        // Which brings down time to O(logn)
+
+        // Implementation :
+
+    }
 }
--- JAVA-8/src/main/java/JAVA_Collection/Java_Collectionss.java
+++ JAVA-8/src/main/java/JAVA_Collection/Java_Collectionss.java
@@ -1,2 +1,49 @@
-package JAVA_Collection;public class Java_Collectionss {
+package JAVA_Collection;
+
+public class Java_Collectionss {
+    public static void main(String[] args) {
+
+        //Class to Class -> Extend KeyWord
+        //Interface to interface -> Extend KeyWord
+        //Class to Interface -> Implements KeyWord
+        //[] -> Interface
+        //() -> class;
+
+
+
+        //        Interface -->                                         [Collection]------------------------------------
+        //                                                              /    |                                          |
+        //                                                             /     |                                          |
+        //                                                            /      |                                          |
+        // Interface-->                                           [Set]   [List]                                        [Queue]
+        // class   -->           (HashSet) (LinkedHashSet) [SortedSet]  (ArrayList)(Vector)(LinkedList)               (LinkedList)(PriorityQueue)
+        //                                                  (TreeSet)
+
+
+
+        // Map is all together a different collection
+        // Hashtable(Class) implements Map
+        // LinkedHashMap(Class) implements Map
+        // HashMap(Class) implements Map
+        // SortedMap(Interface) extends Map(Interface)
+        // Treemap(Class) implements SortedMap(Interface)
+
+        //There is one class called Object
+        // Arrays(Class) extends Object(Class)
+        // Collections(Class) extends Object(class)
+
+
+//        All above data structure are together called collection :
+//        Some common methods are :
+//        1. add
+//        2. addAll
+//        3. remove
+//        4. removeAll
+//        5. size()
+//        6. clear()
+//        7. contains()
+//        8. containsAll()
+//        9. retain()
+//        10. reatinAll()
+    }
 }
